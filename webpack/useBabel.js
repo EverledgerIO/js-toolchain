8a936a9afff0070a3d52657909ed47c0effa3858
pospi <sam@everledger.io>
2016-10-06 17:43:44 +1000

    composable helper for generating babel config for webpack

---
+++ webpack/useBabel.js
@@ -0,0 +1,21 @@
+/**
+ * Configure all necessary options to compile using Babel
+ *
+ * @package: Everledger JS Toolchain
+ * @author:  pospi <sam@everledger.io>
+ * @since:   2016-10-06
+ * @flow
+ */
+
+const { compose, concat, mergeWith } = require('ramda');
+const loader = require('webpack-partial/loader').default;
+
+const addEntrypoints = require('./addEntrypoints');
+
+module.exports = babelSettings => compose(
+  addEntrypoints('babel-polyfill'),
+  loader({
+    loader: 'babel',
+    query: mergeWith(concat, { presets: [], plugins: [] }, babelSettings),
+  })
+);

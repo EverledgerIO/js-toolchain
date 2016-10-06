235f7dd59710805b9efd4ab0a2246299edd0fae2
pospi <sam@everledger.io>
2016-10-06 19:42:06 +1000

    add postcss config defaults to CSS loaders

--- webpack/devCSSLoaders.js
+++ webpack/devCSSLoaders.js
@@ -7,16 +7,30 @@
  * @flow
  */
 
+const { compose } = require('ramda');
 const loader = require('webpack-partial/loader').default;
+const merge = require('webpack-partial/merge').default;
 const postCSSModuleComponents = require('postcss-modules-component-plugin');
 
-module.exports = loader({
-  test: /\.(css|scss)$/,
-  exclude: [/\/node_modules\//],
-  loaders: [
-    { loader: 'style-loader' },
-    { loader: postCSSModuleComponents.loader() },
-    { loader: 'css-loader', query: { sourceMap: true, importLoaders: 1 } },
-    { loader: 'postcss-loader' },
-  ],
-});
+const { syntax, getPostCSSPlugins } = require('../postcss-config');
+
+module.exports = compose(
+  loader({
+    test: /\.(css|scss)$/,
+    exclude: [/\/node_modules\//],
+    loaders: [
+      { loader: 'style-loader' },
+      { loader: postCSSModuleComponents.loader() },
+      { loader: 'css-loader', query: { sourceMap: true, importLoaders: 1 } },
+      { loader: 'postcss-loader' },
+    ],
+  }),
+  merge({
+    postcss: function(webpack) {
+      return {
+        plugins: getPostCSSPlugins(webpack),
+        syntax: syntax,
+      };
+    },
+  })
+);
--- webpack/prodCSSLoaders.js
+++ webpack/prodCSSLoaders.js
@@ -7,24 +7,38 @@
  * @flow
  */
 
+const { compose } = require('ramda');
 const loader = require('webpack-partial/loader').default;
+const merge = require('webpack-partial/merge').default;
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const postCSSModuleComponents = require('postcss-modules-component-plugin');
 
-module.exports = loader({
-  test: /\.(css|scss)$/,
-  exclude: [/\/node_modules\//],
-  loaders: [
-    'style-loader',
-    postCSSModuleComponents.loader(),  // use commonjs output mode for PostCSS modules instead of CSS module format
-    ExtractTextPlugin.extract({
-      // :IMPORTANT: do *not* use fallbackLoader, place your loaders prior to ExtractTextPlugin's
-      fallbackLoader: [],
-      // :IMPORTANT: you must specify loaders as strings here, as ExtractTextPlugin will break with objects
-      loader: [
-        'css-loader?sourceMap=true&importLoaders=1',
-        'postcss-loader',
-      ],
-    }),
-  ],
-});
+const { syntax, getPostCSSPlugins } = require('../postcss-config');
+
+module.exports = compose(
+  loader({
+    test: /\.(css|scss)$/,
+    exclude: [/\/node_modules\//],
+    loaders: [
+      'style-loader',
+      postCSSModuleComponents.loader(),  // use commonjs output mode for PostCSS modules instead of CSS module format
+      ExtractTextPlugin.extract({
+        // :IMPORTANT: do *not* use fallbackLoader, place your loaders prior to ExtractTextPlugin's
+        fallbackLoader: [],
+        // :IMPORTANT: you must specify loaders as strings here, as ExtractTextPlugin will break with objects
+        loader: [
+          'css-loader?sourceMap=true&importLoaders=1',
+          'postcss-loader',
+        ],
+      }),
+    ],
+  }),
+  merge({
+    postcss: function(webpack) {
+      return {
+        plugins: getPostCSSPlugins(webpack, '[emoji][hash:base64:5]'),
+        syntax: syntax,
+      };
+    },
+  })
+);

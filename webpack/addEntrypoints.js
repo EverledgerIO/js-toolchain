b0c8d20c6e20db218c2a7afea4f18dada911aa70
pospi <sam@everledger.io>
2016-10-06 20:01:13 +1000

    document behaviour of entrypoint adder method

--- webpack/addEntrypoints.js
+++ webpack/addEntrypoints.js
@@ -1,6 +1,12 @@
 /**
  * Merge in `entry` array to a webpack config
  *
+ * :WARNING: the order in which you apply multiple calls to this method matters!
+ * Webpack is rather particular about the order of entrypoints.
+ * We do the concatenation in reverse so that entries added later come last, which
+ * means the final (app-specific) entrypoint can be added in the dependant project
+ * after all the core entrypoints have been injected in shared configs.
+ *
  * @see withEntry.js
  *
  * @package: Everledger JS Toolchain

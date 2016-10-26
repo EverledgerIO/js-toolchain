387acd775952ef67035d2eb88d0a5fb5934e16d0
pospi <sam@everledger.io>
2016-10-26 14:47:26 +1000

    workaround an issue with Rollup's support of ES7 async/await

--- rollup/rollup-build-es6-flow.js
+++ rollup/rollup-build-es6-flow.js
@@ -10,6 +10,7 @@
 const { curry } = require('ramda');
 const path = require('path');
 const del = require('del');
+const async = require('rollup-plugin-async');
 const babel = require('rollup-plugin-babel');
 
 function makeRollupConfig(pkg, destDir, entrypoint) {
@@ -19,12 +20,15 @@ function makeRollupConfig(pkg, destDir, entrypoint) {
   return {
     entry: entrypoint,
     external: Object.keys(pkg.dependencies),
-    plugins: [babel(Object.assign(pkg.babel, {
-      babelrc: false,
-      exclude: 'node_modules/**',
-      runtimeHelpers: true,
-      presets: pkg.babel.presets.map(x => (x === 'es2015' ? 'es2015-rollup' : x)),
-    }))],
+    plugins: [
+      async(),
+      babel(Object.assign(pkg.babel, {
+        babelrc: false,
+        exclude: 'node_modules/**',
+        runtimeHelpers: true,
+        presets: pkg.babel.presets.map(x => (x === 'es2015' ? 'es2015-rollup' : x)),
+      })),
+    ],
     targets: [
       {
         format: 'es',
